bbe40ddd8e7425ded0a5ed20f0ce3b10a69072e9
Veysel Bozoglu <aveyselbozoglu@gmail.com>
2021-09-13 23:41:48 +0300

    noktalı kısım hariç tamamlandı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'odev';
+  result: number;
+  container: string = '';
+  containerDisplay: string = '0';
+  first: number;
+  operation: string = '';
+
+
+  // operation değişkeninde yapacağı işlemi tuttum , operasyon seçilince yeni sayı girmeye başlanacağı için first değişkeninde
+  // ilk sayıyı tuttum
+  public operationClicked(e) {
+
+    this.operation = e;
+    this.first = Number(this.containerDisplay)
+    this.container = '';
+  }
+
+  // parametre olan e tıklanan sayı ,container değişkeninde stringi birleştirdim , containerdisplayde ekranda gözüken 
+  public numClicked(e) {
+
+    if (e === 0 && this.container.length < 1)
+      return
+
+    const value: number = e;
+
+    this.container += value.toString();
+
+    this.containerDisplay = this.container;
+
+
+  }
+
+
+  // = tuşuna basınca bu metod çalışır , sonucu resultta tuttum , resultun üzerine işlem yapabilmesi için first değişkenine
+  //   atadım
+  calculateResult() {
+
+    if (this.operation === '+')
+      this.result = this.first + Number(this.container)
+    else if (this.operation === '-')
+      this.result = this.first - Number(this.container)
+    else if (this.operation === '*')
+      this.result = this.first * Number(this.container)
+    else if (this.operation === '/')
+      this.result = this.first / Number(this.container)
+
+    this.first = this.result
+
+    this.containerDisplay = this.container = this.result.toString()
+
+    this.result = null;
+
+  }
+
+  // bütün değişkenleri 0lar ,yani başlangıç anına geri döner
+  allClear() {
+
+    this.result = 0;
+    this.container = '';
+    this.containerDisplay = '0';
+    this.first = 0;
+    this.operation = '';
+
+  }
 }
